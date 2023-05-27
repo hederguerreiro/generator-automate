9f833ba924c138c2739588c3bb14516ff3b2490b
Heder Jaelson Guerreiro <heder.guerreiro@gmail.com>
2023-05-27 09:08:27 -0300

    Unit test, prettier configured and CI.

--- generators/app/index.js
+++ generators/app/index.js
@@ -22,44 +22,44 @@
  * THE SOFTWARE.
  */
 
-'use strict';
+"use strict";
 
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
 
 module.exports = class extends Generator {
+  prompting() {
+    this.log(
+      yosay(
+        `Welcome to ${chalk.blue("automate")} generator! 
+                Create test automation projects for E2E tests. Test Frameworks supported: ${chalk.green(
+                  "Selenium"
+                )}`
+      )
+    );
 
-    prompting() {
+    const prompts = [
+      {
+        type: "list",
+        name: "framework",
+        message: "Select a framework:",
+        choices: [
+          {
+            name: "Selenium",
+            value: "selenium"
+          }
+        ],
+        default: "selenium"
+      }
+    ];
 
-        this.log(
-            yosay(
-                `Welcome to ${chalk.blue('automate')} generator! 
-                Create test automation projects for E2E tests. Test Frameworks supported: ${chalk.green('Selenium')}`
-            )
-        );
+    return this.prompt(prompts).then(props => {
+      this.props = props;
+    });
+  }
 
-        const prompts = [
-            {
-                type: 'list',
-                name: 'framework',
-                message: 'Select a framework:',
-                choices: [
-                    {
-                        name: 'Selenium',
-                        value: 'selenium',
-                    }
-                ]
-            }
-        ];
-
-        return this.prompt(prompts).then(props => {
-            this.props = props;
-        });
-    }
-
-    intializing() {
-        this.composeWith(require.resolve(`../${this.props.framework}`));
-    }
-
+  intializing() {
+    this.composeWith(require.resolve(`../${this.props.framework}`));
+  }
 };
--- generators/selenium/index.js
+++ generators/selenium/index.js
@@ -22,98 +22,98 @@
  * THE SOFTWARE.
  */
 
-'use strict';
+"use strict";
 
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
 
 module.exports = class extends Generator {
+  prompting() {
+    this.log(
+      yosay(`Prepare to create your ${chalk.green("Selenium Java project")}!`)
+    );
 
-    prompting() {
+    const defaultPrompts = [
+      {
+        type: "confirm",
+        name: "isNewProject",
+        message: "Are you going to create a new Project?",
+        default: true
+      }
+    ];
 
-        this.log(
-            yosay(
-                `Prepare to create your ${chalk.green('Selenium Java project')}!`
-            )
-        );
+    const projectConfigPrompts = [
+      {
+        type: "input",
+        name: "projectName",
+        message: "What is the name of the project?"
+      },
+      {
+        type: "input",
+        name: "projectPath",
+        message: "What is the path to the project?",
+        default: __dirname
+      },
+      {
+        type: "checkbox",
+        name: "buildAutomation",
+        message: "Choose the build automation:",
+        choices: [
+          { name: "Maven", value: "maven" },
+          { name: "Gradle", value: "gradle" },
+          { name: "None", value: "none", checked: true }
+        ]
+      },
+      {
+        type: "checkbox",
+        name: "framework",
+        message: "Choose the Test Framework:",
+        choices: [
+          { name: "JUnit", value: "junit" },
+          { name: "TestNG", value: "testNG" },
+          { name: "No test framework", value: "none", checked: true }
+        ]
+      },
+      {
+        type: "checkbox",
+        name: "report",
+        message: "Choose the reporter:",
+        choices: [
+          { name: "TestNG Reporter", value: "testNGReporter" },
+          { name: "Allure Report", value: "allureReport" },
+          { name: "No reporter tool", value: "none", checked: true }
+        ]
+      }
+    ];
 
-        const defaultPrompts = [
-            {
-                type: 'confirm',
-                name: 'isNewProject',
-                message: 'Are you going to create a new Project?',
-                default: true
-            }
-        ];
-
-        const projectConfigPrompts = [
-            {
-                type: 'input',
-                name: 'projectName',
-                message: 'What is the name of the project?'
-            },
-            {
-                type: 'input',
-                name: 'projectPath',
-                message: 'What is the path to the project?',
-                default: __dirname
-            },
-            {
-                type: 'checkbox',
-                name: 'buildAutomation',
-                message: 'Choose the build automation:',
-                choices: [
-                    { name: 'Maven', value: 'maven' },
-                    { name: 'Gradle', value: 'gradle' },
-                    { name: 'None', value: 'none', checked: true },
-                ]
-            },
-            {
-                type: 'checkbox',
-                name: 'framework',
-                message: 'Choose the Test Framework:',
-                choices: [
-                    { name: 'JUnit', value: 'junit' },
-                    { name: 'TestNG', value: 'testNG' },
-                    { name: 'No test framework', value: 'none', checked: true},
-                ]
-            },
-            {
-                type: 'checkbox',
-                name: 'report',
-                message: 'Choose the reporter:',
-                choices: [
-                    { name: 'TestNG Reporter', value: 'testNGReporter' },
-                    { name: 'Allure Report', value: 'allureReport' },
-                    { name: 'No reporter tool', value: 'none', checked: true },
-                ]
-            }
-        ];
-
-        return this.prompt(defaultPrompts).then(props => {
-            this.defaultProps = props;
-            if (this.defaultProps.isNewProject) {
-                return this.prompt(projectConfigPrompts).then(props2 => {
-                    this.projectConfigProps = props2;
-                });
-            } else {
-                console.log('Ok, there is no other option to choose beside this. Tks!');
-            }
+    return this.prompt(defaultPrompts).then(props => {
+      this.defaultProps = props;
+      if (this.defaultProps.isNewProject) {
+        return this.prompt(projectConfigPrompts).then(props2 => {
+          this.projectConfigProps = props2;
         });
-    }
+      }
+    });
+  }
 
-    writing() {
-        if (this.projectConfigProps !== undefined) {
-            console.log(`New project name: ${this.projectConfigProps.projectName}`);
-            console.log(`New project path: ${this.projectConfigProps.projectPath}`);
-            console.log(`New project build automation: ${this.projectConfigProps.buildAutomation}`);
-            console.log(`New project build framework: ${this.projectConfigProps.framework}`);
-            console.log(`New project build report tool: ${this.projectConfigProps.report}`);
-        }
+  writing() {
+    if (this.projectConfigProps !== undefined) {
+      console.log(`New project name: ${this.projectConfigProps.projectName}`);
+      console.log(`New project path: ${this.projectConfigProps.projectPath}`);
+      console.log(
+        `New project build automation: ${this.projectConfigProps.buildAutomation}`
+      );
+      console.log(
+        `New project build framework: ${this.projectConfigProps.framework}`
+      );
+      console.log(
+        `New project build report tool: ${this.projectConfigProps.report}`
+      );
     }
+  }
 
-    install() {
-        // TODO document this is not ready yet
-    }
+  install() {
+    // TODO document this is not ready yet
+  }
 };
